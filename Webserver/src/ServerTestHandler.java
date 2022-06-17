029a42bc8f5722623bcb3d2a4dd2c56929a02260
William Ng <vvilan@gmail.com>
2022-06-17 22:15:20 +0100

    added server and assorted eclipse configuration

---
+++ Webserver/src/ServerTestHandler.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+import java.io.OutputStream;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class ServerTestHandler implements HttpHandler {
+  @Override    
+  public void handle(HttpExchange httpExchange) throws IOException {
+    if ("GET".equals(httpExchange.getRequestMethod())) {
+      handleResponse(httpExchange); 
+    } else {
+      throw new UnsupportedOperationException("Only get request are supported");
+    }
+  }
+
+  private void handleResponse(HttpExchange httpExchange) throws IOException {
+    byte[] htmlResponse = "hello".getBytes();
+    
+    httpExchange.getResponseHeaders().add("Access-Control-Allow-Origin", "*");
+    httpExchange.getResponseHeaders().add("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+    httpExchange.getResponseHeaders().add("Access-Control-Allow-Credentials", "true");
+    httpExchange.getResponseHeaders().add("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization");
+    httpExchange.sendResponseHeaders(200, htmlResponse.length);
+    OutputStream outputStream = httpExchange.getResponseBody();
+    outputStream.write(htmlResponse);
+    outputStream.flush();
+    outputStream.close();
+  }
+}
+
--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,10 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [success, setSuccess] = useState(false);
+  
+  // Checks if the server is running
+  useEffect(() => {
+    fetch('test', { method: "GET" } )
+      .then((response) => {
+        const success = response.status == 200 ? true : false;
+        setSuccess(success);
+      });
+  }, []);
+  
   return (
     <div className="App">
-     	Hello world
+      {success ? "Successfully connected to server!" : "Unable to connect to server, is the server running?"}
     </div>
   );
 }
