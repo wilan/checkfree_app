76447a8a7cb19ce39ccd3195084b1a6dc3f644df
Jeffrey Wu <jeffreywu@new-host-5.home>
2022-07-27 00:16:57 -0400

    milesstone 1

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useState, useEffect } from 'react';
 
+
 function App() {
   const [success, setSuccess] = useState(false);
   
@@ -8,16 +9,48 @@ function App() {
   useEffect(() => {
     fetch('test', { method: "GET" } )
       .then((response) => {
-        const success = response.status == 200 ? true : false;
+        const success = response.status === 200 ? true : false;
         setSuccess(success);
       });
   }, []);
-  
+
   return (
     <div className="App">
+         <table style={{ "margin-bottom": "20px"}} >
+      <th>
+          <tr>
+              <td>List</td>
+              <td></td>
+          </tr>
+      </th>
+      <tbody>
+          <tr>
+              <td>Item 1</td>
+              <td><button>X</button></td>
+          </tr>
+          <tr>
+              <td>Item 2</td>
+              <td><button>X</button></td>
+          </tr>
+          <tr>
+              <td>...</td>
+              <td><button>X</button></td>
+          </tr>
+      </tbody>
+  </table>
+
+  <div style= {{"fomargin-bottom" : "20px"}} >
+      <label>Add Item:</label>
+      <input type="text" name="name" />
+  </div>
+  <br/>
+  <button>Add</button>
+  <button>Refresh List</button>
       {success ? "Successfully connected to server!" : "Unable to connect to server, is the server running?"}
     </div>
   );
 }
 
 export default App;
+
+
